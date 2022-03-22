a3a25c5ea53e78a2358cbd66064a78a40be9d573
Devlee247 <inseo8070@naver.com>
2022-03-22 06:48:27 +0000

    [refact] 불피요한 파일 삭제

---
+++ mmdetection/configs/exp1/dataset.py
@@ -0,0 +1,60 @@
+# dataset settings
+
+dataset_type = 'CocoDataset'
+data_root = 'dataset/'
+
+classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
+           "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 1024),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'train_fold0.json',
+        img_prefix=data_root,
+        pipeline=train_pipeline,
+        classes=classes,
+        ),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'val_fold0.json',
+        img_prefix=data_root,
+        pipeline=test_pipeline,
+        classes=classes,
+        ),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'test.json',
+        img_prefix=data_root,
+        pipeline=test_pipeline,
+        classes=classes,
+        ))
+evaluation = dict(interval=1, metric='bbox', save_best='bbox_mAP_50')
